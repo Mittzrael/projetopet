22dc2e57a938bfca88c4a67483c99df892065234
Andre <jomonzito2010@gmail.com>
2019-02-20 17:07:55 -0300

    SoundManager iniciado

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+    private AudioSource backgroundAudioSource;
+    private AudioSource sfxAudioSource;
+    private AudioSource animalAudioSource;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void SoundManagerOnTransition()
+    {
+        backgroundAudioSource.Stop();
+        sfxAudioSource.Stop();
+        animalAudioSource.Stop();
+    }
+
+    /* Fazer função para mudar o volume.
+     * Função de cada play, recebendo um audioClip
+     */
+}
