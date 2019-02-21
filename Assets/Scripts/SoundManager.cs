dba270412d43845f21703e139e7f91e32cd38e6b
gustavo <gustavo.castilho@ufabc.edu.br>
2019-02-21 15:20:35 -0300

    SoundManager

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     public static SoundManager instance;
+    [SerializeField]
     private AudioSource backgroundAudioSource;
+    [SerializeField]
     private AudioSource sfxAudioSource;
+    [SerializeField]
     private AudioSource animalAudioSource;
 
     private void Awake()
@@ -22,6 +25,9 @@ public class SoundManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
+    /// <summary>
+    /// Stops all music and sounds currently being reproduced.
+    /// </summary>
     public void StopAllSounds()
     {
         backgroundAudioSource.Stop();
@@ -29,55 +35,90 @@ public class SoundManager : MonoBehaviour
         animalAudioSource.Stop();
     }
 
+    /// <summary>
+    /// Plays a background music.
+    /// </summary>
+    /// <param name="backgroundMusic">AudioClip to be reproduced.</param>
     public void PlayBackgroundMusic(AudioClip backgroundMusic)
     {
         backgroundAudioSource.clip = backgroundMusic;
         backgroundAudioSource.Play();
     }
 
+    /// <summary>
+    /// Plays a Sound Effect.
+    /// </summary>
+    /// <param name="sfx">AudioClip to be reproduced.</param>
     public void PlaySFX(AudioClip sfx)
     {
         sfxAudioSource.clip = sfx;
         sfxAudioSource.Play();
     }
 
+    /// <summary>
+    /// Plays a sound effect for animal sounds.
+    /// </summary>
+    /// <param name="animalSound">AudioClip to be reproduced.</param>
     public void PlayAnimalSound(AudioClip animalSound)
     {
         animalAudioSource.clip = animalSound;
         animalAudioSource.Play();
     }
 
+    /// <summary>
+    /// Sets MUTE = true to background music.
+    /// </summary>
     public void MuteBackgroundMusic()
     {
         backgroundAudioSource.mute = true;
     }
 
+    /// <summary>
+    /// Sets MUTE = true to all sfx.
+    /// </summary>
     public void MuteSFX()
     {
         sfxAudioSource.mute = true;
         animalAudioSource.mute = true;
     }
 
+    /// <summary>
+    /// Changes background music volume.
+    /// </summary>
+    /// <param name="volume">Float 0 to 1.</param>
     public void ChangeBackgroundVolume(float volume)
     {
         backgroundAudioSource.volume = volume;
     }
 
+    /// <summary>
+    /// Stops background music.
+    /// </summary>
     public void StopBackgroundMusic()
     {
         backgroundAudioSource.Stop();
     }
 
+    /// <summary>
+    /// Stops SFX.
+    /// </summary>
     public void StopAnimalSound()
     {
         animalAudioSource.Stop();
     }
 
+    /// <summary>
+    /// Stops SFX for animal sounds.
+    /// </summary>
     public void StopSFXSound()
     {
         sfxAudioSource.Stop();
     }
 
+    /// <summary>
+    /// Check if there is any background music currently playing.
+    /// </summary>
+    /// <returns>Returns true if something is being played or false if nothing is playing.</returns>
     public bool IsBackgroundPlaying()
     {
         if (backgroundAudioSource.isPlaying)
@@ -89,9 +130,4 @@ public class SoundManager : MonoBehaviour
             return false;
         }
     }
-
-
-    /* Fazer função para mudar o volume.
-     * Função de cada play, recebendo um audioClip
-     */
 }
