39fa8bbfe1d77f5e827952152d38ea335b8e1f92
Rafael Nicoletti <40860359+RafaelGNicoletti@users.noreply.github.com>
2019-02-21 16:57:50 -0300

    Scene02

---
+++ Assets/Scripts/DragMenuScripts/MenuItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class MenuItem
+{
+    public Sprite itemSprite;
+    public Texture itemTexture;
+    public string itemName;
+}
---
+++ Assets/Scripts/DragMenuScripts/MenuManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    private MenuItem[] itensToShow;
+    public MenuItemList itemList;
+
+    public Transform contentPanel;
+    private GameObject itemInstance;
+    public GameObject itemPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        itensToShow = itemList.items;
+        ShowItens();
+    }
+
+    private void ShowItens()
+    {
+        Debug.Log(itemList.items.Length);
+        for (int i = 0; i < itensToShow.Length; i++)
+        {
+            itemInstance = Instantiate(itemPrefab, GameObject.Find("AvatarPanel").transform);
+            DisplayItemInfo(i);
+        }
+    }
+
+    private void DisplayItemInfo(int i)
+    {
+
+    }
+}
