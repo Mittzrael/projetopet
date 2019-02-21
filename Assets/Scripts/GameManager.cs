27c5dfb9ecb197d47b65028c7c260b4084199b0d
Andre <jomonzito2010@gmail.com>
2019-02-21 15:21:34 -0300

    Fade adicionado, GameManager integrado pelo LoadScene with Fade e com o Loader de outrora.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,5 +4,69 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance = null;
+    GameManager gameManager;
+    SoundManager soundManager;
+    AnimationManager animManager;
 
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    public static void InitializeManagers()
+    {
+        GameObject videoManager, gameManager, animManager, soundManager;
+
+        videoManager = Resources.Load("Prefabs/VideoManager") as GameObject;
+        gameManager = Resources.Load("Prefabs/GameManager") as GameObject;
+        animManager = Resources.Load("Prefabs/AnimationManager") as GameObject;
+        soundManager = Resources.Load("Prefabs/SoundManager") as GameObject;
+
+        if (VideoManager.instance == null)
+        {
+            Instantiate(videoManager);
+        }
+
+        if (GameManager.instance == null)
+        {
+            Instantiate(gameManager);
+        }
+
+        if(AnimationManager.instance == null)
+        {
+            Instantiate(animManager);
+        }
+
+        if(SoundManager.instance == null)
+        {
+            Instantiate(soundManager);
+        }
+    }
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    /// <summary>
+    /// Função para carregar uma nova fase com fade - em construção
+    /// </summary>
+    /// <param name="Scene"></param>
+    public void LoadSceneWithFade(string scene)
+    {
+        animManager = AnimationManager.instance;
+        soundManager = SoundManager.instance;
+        //SilabaControl silabaControl = SilabaControl.instance;
+        //silabaControl.StopCoroutines();
+        //StopAllCoroutines();
+        soundManager.StopAllSounds();
+        animManager = GameObject.FindGameObjectWithTag("AnimationManager").GetComponent<AnimationManager>();
+        StartCoroutine(animManager.Fade(scene));
+    }
 }
