6d73dea825291181d0f51044db473bdaa20df0e5
gustavo <gustavo.castilho@ufabc.edu.br>
2019-02-22 14:21:06 -0300

    Options Menu.

---
+++ Assets/Scripts/BotoesMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotoesMenu : MonoBehaviour
+{
+    GameObject menuOptions;
+
+    void Start()
+    {
+        menuOptions = Resources.Load("Prefabs/SubMenus/MenuOptions") as GameObject;
+    }
+
+    public void OpenOptionsMenu()//abre menu opções menu principal
+    {
+        if (GameObject.Find("MenuOptions(Clone)") == null)
+        {            
+            GameObject newOptionsMenu;
+            newOptionsMenu = Instantiate(menuOptions);
+            newOptionsMenu.transform.SetParent(GameObject.Find("Canvas").transform, false);
+        }
+    }
+}
---
+++ Assets/Scripts/CursorChange.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+/// <summary>
+/// Esta classe só pode ser anexada a UI Buttons. 
+/// Quando o mouse passa por cima do objeto anexado, o cursor alterna para a imagem aqui inserida.
+/// O botão ao qual este script estiver anexado vai tocar um CLICK quando for apertado.
+/// Também pode ser herdada.
+/// </summary>
+
+public class CursorChange : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Texture2D cursor;
+    private Button botao;
+    AudioClip clickSound;
+
+    private void Awake()
+    {
+        cursor = Resources.Load<Texture2D>("Images/cursor-edit-th");
+        clickSound = (AudioClip)Resources.Load("Sounds/sfx/click_tecla01");
+        botao = this.GetComponent<UnityEngine.UI.Button>();
+    }
+
+    public void Start()
+    {
+        botao.onClick.AddListener(delegate() { SoundManager.instance.PlaySFX(clickSound); });
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (botao.interactable) {
+            Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+}
