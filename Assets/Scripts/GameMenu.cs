bc6f1bece508e686d8b7d14d14bf1728fc27d8a2
JopaFM <joao.paulo.fm@hotmail.com>
2019-02-25 17:57:30 -0300

    Teste Menu

---
+++ Assets/Scripts/GameMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMenu : MonoBehaviour
+{
+    public GameObject btnInventario, btnPetShop, btnAcaoPet;
+    bool isMenuOpen = false;
+
+    public void OnClickBtnMenu()
+    {
+        if (!isMenuOpen)
+        {
+            btnInventario.SetActive(true);
+            btnPetShop.SetActive(true);
+            btnAcaoPet.SetActive(true);
+            isMenuOpen = true;
+        }
+        else
+        {
+            btnInventario.SetActive(false);
+            btnPetShop.SetActive(false);
+            btnAcaoPet.SetActive(false);
+            isMenuOpen = false;
+        }
+    }
+
+}
