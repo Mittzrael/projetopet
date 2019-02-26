e8e01c2310ec07071bc0c7f41a8eff38339dcdb0
gustavo <gustavo.castilho@ufabc.edu.br>
2019-02-26 14:21:32 -0300

    inicio Tela dos erros iniciais.unity

---
+++ Assets/Scripts/Stage1Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage1Controller : MonoBehaviour
+{
+    public string[] dialogoInicial;
+    public Texture texture;
+    public string[] dialogoAindaTemErros;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextPanelController.CreateDialogBox(dialogoInicial, new Vector3(205, 75, 0), texture);
+    }
+
+    public void VerificarErros()
+    {
+        TextPanelController.CreateDialogBox(dialogoAindaTemErros, new Vector3(205, 75, 0), texture);
+    }
+}
