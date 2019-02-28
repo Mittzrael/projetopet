5eefce2d93f7bcabd9faf28246b2ecc54daaf58f
Andre <jomonzito2010@gmail.com>
2019-02-28 16:32:27 -0300

    Criado e implementado barras de necessidades para o pet.

--- Assets/Scripts/SaveScripts/Player.cs
+++ Assets/Scripts/SaveScripts/Player.cs
@@ -8,7 +8,7 @@ public class Player
 {
     public int slot = -1;
     public string nome = "";
-
+    public Health health = new Health();
     public int avatarSelecionado = -1;
     public int residenciaSelecionada = -1;
     public int petSelecionado = -1;
