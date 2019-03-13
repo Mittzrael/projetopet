dcbacd5628eda4469a6c38cf643cc327f0295de8
gustavo <gustavo.castilho@ufabc.edu.br>
2019-03-13 11:46:47 -0300

    Inicio outras cenas cozinha e quintal.

---
+++ Assets/Scripts/Doors.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour
+{
+    public string sceneToLoad;
+
+    private void OnMouseUpAsButton()
+    {
+        GameManager.instance.LoadSceneWithFade(sceneToLoad);
+        GameObject.Find("dog_mitza").transform.position = new Vector3(0,-630,0);//ajusta posição na próxima tela, pode ser removido se os backgrounds forem padronizadas
+    }
+}
