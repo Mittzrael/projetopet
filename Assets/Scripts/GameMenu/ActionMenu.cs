e8fe224618bd9046f015d3ec4424b5dc026985c0
JopaFM <joao.paulo.fm@hotmail.com>
2019-03-14 17:39:00 -0300

    Base do menu inferior implementada

---
+++ Assets/Scripts/GameMenu/ActionMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Script para o menu de ações 
+/// Centraliza as ações de interação com o pet
+/// </summary>
+
+public class ActionMenu : MonoBehaviour
+{
+    public void OnBtnChamarPetClick()
+    {
+        Debug.Log("Chamar pet");
+    }
+    public void OnBtnAcariciarPetClick()
+    {
+        Debug.Log("Acariciar pet");
+    }
+    public void OnBtnEnsinarPetClick()
+    {
+        Debug.Log("Ensinar pet");
+    }
+}
---
+++ Assets/Scripts/GameMenu/InventoryDrag.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryDrag : MonoBehaviour
+{
+    public GameObject item;
+
+    public void OnItemMousedown()
+    {
+        Debug.Log("oi");
+        //Instancia o itemDrag
+        Vector3 clickedPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        clickedPosition.z = 0;
+        Instantiate(item, clickedPosition, Quaternion.identity);
+    }
+}
