b91914dd01e9f932c475f52e26a2e94e6f1b026d
JopaFM <joao.paulo.fm@hotmail.com>
2019-03-25 17:53:48 -0300

    Drop Area funcionando.

---
+++ Assets/Scripts/GameMenu/DropArea.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropArea : MonoBehaviour
+{
+    static string currentDropArea = "null";
+    string dropAreaName;
+
+    private void Start()
+    {
+        dropAreaName = this.name;
+    }
+
+    public void OnDropAreaEnter()
+    {
+        currentDropArea = dropAreaName;
+    }
+
+    public void OnDropAreaExit()
+    {
+        currentDropArea = "null" ;
+    }
+
+    public static string GetCurrentDropArea()
+    {
+        return currentDropArea;
+    }
+}
