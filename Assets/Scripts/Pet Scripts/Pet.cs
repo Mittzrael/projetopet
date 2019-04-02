da869e33b5987c1b04ad2d2104a569e2104d6e5c
Andre <jomonzito2010@gmail.com>
2019-04-02 17:48:30 -0300

    Pet atualizado

--- Assets/Scripts/Pet Scripts/Pet.cs
+++ Assets/Scripts/Pet Scripts/Pet.cs
@@ -49,33 +49,63 @@ public class Pet: MonoBehaviour
     /// <summary>
     /// TODO: Coco e xixi em lugares aleatórios
     /// </summary>
-    public void PeeOnLocation()
+    public IEnumerator PeeOnLocation()
     {
         pee = Resources.Load("Prefabs/Items/Pee") as GameObject;
         Vector3 position = new Vector3(transform.position.x, transform.position.y - GetComponent<Renderer>().bounds.size.y / 2, transform.position.z-5); //Eixo Z tem que ser menor para ficar mais perto da câmera e ativar o OnMouseDown()
         Instantiate(pee, position, Quaternion.identity);
         SaveManager.instance.player.health.PutInPee(-0.5f); //Esvazia pela metade a vontade do animal de fazer xixi
         SaveManager.instance.player.peeLocation.Add(SceneManager.GetActiveScene().name, position);
+        yield return new WaitForSeconds(0);
     }
 
     /// <summary>
     /// Chamado quando o animal evacua.
     /// </summary>
-    public void PoopOnLocation()
+    public IEnumerator PoopOnLocation()
     {
         poop = Resources.Load("Prefabs/Items/Poop") as GameObject;
         Vector3 position = new Vector3(transform.position.x, transform.position.y - GetComponent<Renderer>().bounds.size.y/2, transform.position.z-5);
         Instantiate(poop, position, Quaternion.identity);
         SaveManager.instance.player.health.PutInPoop(-0.5f); //Esvazia pela metade a vontade do animal de fazer cocô
         SaveManager.instance.player.poopLocation.Add(SceneManager.GetActiveScene().name, position);
+        yield return new WaitForSeconds(0);
     }
 
     public void PoopRandomPlace()
     {
-        string sceneName = "Nome aleatório";
-
+        string sceneName = ReturnSceneName();
+        Vector3 position = randomPosition(sceneName); 
         //SaveManager.instance.player.poopLocation.Add(sceneName, );
     }
+
+    private Vector3 randomPosition(string scene)
+    {
+        Vector3 position = new Vector3();
+        int upperBound;
+        int lowerBound;
+
+
+
+        position.x = 
+    }
+
+    public string ReturnSceneName()
+    {
+        int random = (int)Random.Range(1, 2);
+
+        switch (random)
+        {
+            case 1:
+                return "MainRoom";
+            case 2:
+                return "Yard";
+            default:
+                return "";
+        }
+    }
+
+    public 
 
     public void PeeRandomPlace()
     {
