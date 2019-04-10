af02974a397daaee9dbb77933c03ca6be8e2a142
Andre <jomonzito2010@gmail.com>
2019-04-10 16:55:20 -0300

    Iniciado TimeManager

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    TimeManager instance = null;
+    [SerializeField]
+    private int currentPeriod;
+    [SerializeField]
+    private double[] timeBetweenPer;
+    [SerializeField]
+    private int numberOfPeriods;
+    [SerializeField]
+    private double limitTime;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+    /*
+    public void ForwardToNextPeriod()
+    {
+
+    }
+
+    public void ResetPeriod()
+    {
+
+    }
+
+    public IEnumerator ResetTimeCounter()
+    {
+
+    }
+
+    public IEnumerator ResetTimeCounter(double time)
+    {
+
+    }
+
+    public IEnumerator PeriodTimeCounter()
+    {
+
+    }
+
+    public IEnumerator PeriodTimeCounter(double time)
+    {
+
+    }
+
+    public void TimeBetweenSessions()
+    {
+
+    }
+    */
+}
