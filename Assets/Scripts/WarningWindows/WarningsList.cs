5ffa9cec87cb6d276088f3724e4fef77f58a477b
Rafael Nicoletti <40860359+RafaelGNicoletti@users.noreply.github.com>
2019-04-12 16:49:15 -0300

    UI

---
+++ Assets/Scripts/WarningWindows/WarningsList.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WarningsList", menuName = "My Assets/Lista de Avisos PopUp")]
+public class WarningsList : ItemList
+{
+    public Warnings[] warnings;
+}
